db13d2806292922917d1bc0f4d6df992c0f90155
sylvester <sylvestermbah90@gmail.com>
2026-07-10 02:36:48 -0700

    Add share rootlayout with navbar,footer and outlet

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,18 @@
-
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <main className="flex-grow">
+            <Outlet />
+        </main>
+
+        <Footer />
+    </div>
   )
 }
 
