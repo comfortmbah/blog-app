a2568ebf89bce5fb20fa4bde094d22b9fec06156
sylvester <sylvestermbah90@gmail.com>
2026-07-10 05:27:19 -0700

    Add responsive reusable footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="mt-12 border-t bg-gray-100">
+      <div className="mx-auto max-w-7xl flex items-center flex-col justify-between text-sm py-4 px-6 text-gray-600 md:flex-row">
+        <p className="text-xl">&copy; {new Date().getFullYear()} BlogApp. All right reserved.</p>
+        <p className="text-xl">Built with React & Tailwind CSS.</p>
+      </div>
+    </footer>
   )
 }
 
