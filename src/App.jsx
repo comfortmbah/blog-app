ff5d417c775021653a5d148a8ba8cbd02fe0fc79
sylvester <sylvestermbah90@gmail.com>
2026-07-10 02:28:11 -0700

    Set up application routing with nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import BlogDetails from './pages/BlogDetails'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
+import RootLayout from './layouts/RootLayout'
+import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements} from 'react-router-dom'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path='blogs' element={<Blogs />} />
+        <Route path='blogs/:id' element={<BlogDetails />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    )
+  );
+
   return (
-    <div className='text-6xl font-bold text-center text-red-500'>Blog App</div>
+    <RouterProvider router={router} />
   )
 }
 
