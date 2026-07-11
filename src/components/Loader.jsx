e92f3a93020760563d65056744af83d953c60bc2
sylvester <sylvestermbah90@gmail.com>
2026-07-11 07:35:40 -0700

    Create reusable loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center py-12">
+        <p className="text-lg font-medium text-gray-600">
+            Loading...
+        </p>
+    </div>
+  )
+}
+
+export default Loader
