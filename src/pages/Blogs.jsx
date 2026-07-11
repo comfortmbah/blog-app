c69c752e9d06e6e3caac64df235ec7297e41018b
sylvester <sylvestermbah90@gmail.com>
2026-07-11 08:59:14 -0700

    Display blog posts using custom useFetch hook

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,6 +1,18 @@
-
+import BlogCard from "../components/BlogCard"
+import Error from "../components/Error"
+import Loader from "../components/Loader"
+import useFetch from "../hooks/useFetch"
 
 const Blogs = () => {
+  const { data, loading, error } = useFetch("https://dummyjson.com/posts");
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <Error message={error} />
+  }
   return (
     <section className="mx-auto max-w-7xl px-6 py-12">
       <div className="mb-10">
@@ -13,7 +25,14 @@ const Blogs = () => {
         </p>
       </div>
 
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3"></div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {data.posts.map((blog) => (
+          <BlogCard 
+            key={blog.id}
+            blog={blog}
+          />
+        ))}
+      </div>
     </section>
   )
 }
