e871fb8f913e3e223e13e81ee1af3c025398c692
sylvester <sylvestermbah90@gmail.com>
2026-07-14 11:54:08 -0700

    Add useState variable

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -3,11 +3,13 @@ import Error from "../components/Error"
 import Loader from "../components/Loader"
 import useFetch from "../hooks/useFetch"
 import SearchBar from "../components/SearchBar"
-import { useState } from "react"
+import { useState, useMemo } from "react"
 import EmptyState from "../components/EmptyState"
+import CategoryFilter from "../components/CategoryFilter"
 
 const Blogs = () => {
   const [searchItem, setSearchItem] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
   const { data, loading, error } = useFetch("https://dummyjson.com/posts");
 
   if (loading) {
